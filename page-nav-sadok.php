fb1c1183556090410d561e6b30ee26e001c9b74c
nk44ukrnet <56731417+nk44ukrnet@users.noreply.github.com>
2022-07-05 16:52:59 +0300

    templates added

---
+++ page-nav-sadok.php
@@ -0,0 +1,30 @@
+<?php
+/*
+* Template name: Stemchik-sadok-nav-page
+*/
+get_header();
+?>
+
+<section class="bg_light">
+    <div class="__container">
+        <h1 class="heading mt-3 text-md-start text-sm-center ">Садочок</h1>
+        <div class="menu">
+            <div class="menu__holder">
+                <a href=#!" class="menu__item menu__link"><span class="menu__text">Вікові групи</span></a>
+            </div>
+            <div class="menu__holder">
+                <div class="menu__item">Освітня програма
+                    <ul>
+                        <li><a href="#!" class="menu__link menu__text">Приклад розкладу</a></li>
+                        <li><a href="#!" class="menu__link menu__text">Орієнтовний розпорядок дня</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="menu__holder">
+                <a href="#!" class="menu__item menu__link">Організація харчування</a>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php get_footer(); ?>
---
+++ page-sadok-age-group.php
@@ -0,0 +1,47 @@
+<?php
+/*
+* Template name: Stemchik-sadok-age-group
+*/
+get_header();
+?>
+
+<section class="bg_light">
+    <div class="__container mb-5 mt-5">
+        <h1 class="heading">Вікові групи</h1>
+        <p>
+            Садок має дві вікові групи: для дітей 4-5 та 5-6 років.  <br>
+            Заняття в групах відрізняються з урахуванням віку дітей та їхньго розвитку у відповідному віці. 
+        </p>
+        <div class="menu no_hover">
+            <div class="menu__holder max-width-sm">
+                <h2 class="heading_sm border_bottom">Групи для дітей 4-5 років  включають такі навчальні заняття: 
+                </h2>
+                <ul>
+                    <li>Ейдетика та логоритміка </li>
+                    <li>Дослідження довкілля </li>
+                    <li>Пізнання STEAM світу </li>
+                    <li>Грамота </li>
+                    <li>Математика </li>
+                    <li>Творче конструювання 
+                    </li>
+                </ul>
+            </div>
+            <div class="menu__holder max-width-sm">
+                <h2 class="heading_sm border_bottom">Групи для дітей 5-6 років  включають такі навчальні заняття: 
+                </h2>
+                <ul>
+                    <li>Грамота</li>
+                    <li>Математика</li>
+                    <li>Творче конструювання </li>
+                    <li>Ейдетика та логоритміка </li>
+                    <li>Арт-простір </li>
+                    <li>Експерименти та відкриття </li>
+                    <li>Англійська мова </li>
+                    <li>Програмування у грі </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php get_footer(); ?>
