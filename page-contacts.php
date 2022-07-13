37a88d96f4ea82b191f0a626dc3e7b32738482ca
nk44ukrnet <56731417+nk44ukrnet@users.noreply.github.com>
2022-07-13 17:09:40 +0300

    contacts page edits

--- page-contacts.php
+++ page-contacts.php
@@ -9,7 +9,7 @@ get_header();
     <div class="__container mt-3 mb-5">
         <h1 class="heading">Локації та контакти</h1>
         <div data-location class="border_bottom">
-            <h2 class="border_bottom">м. Дніпро</h2>
+            <h2>м. Дніпро</h2>
             <p>Вул. Яворницького, 67</p>
             <iframe class="iframe-responsive"
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2645.516380545511!2d35.04043561584305!3d48.46580803622145!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40dbe2e656c44d5b%3A0xc71b1976fd82f1aa!2z0L_RgNC-0YHQv9C10LrRgiDQlNC80LjRgtGA0LAg0K_QstC-0YDQvdC40YbRjNC60L7Qs9C-LCA2Nywg0JTQvdGW0L_RgNC-LCDQlNC90ZbQv9GA0L7Qv9C10YLRgNC-0LLRgdGM0LrQsCDQvtCx0LvQsNGB0YLRjCwgNDkwMDA!5e0!3m2!1suk!2sua!4v1657718010792!5m2!1suk!2sua"
@@ -18,7 +18,7 @@ get_header();
         </div>
 
         <div data-location class="border_bottom">
-            <h2 class="border_bottom">м. Чернівці</h2>
+            <h2>м. Чернівці</h2>
             <p>Вул. Ентузіастів, 8</p>
             <iframe class="iframe-responsive border_bottom" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2656.267632590066!2d25.9447493158378!3d48.25922395065921!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47340f2c6ec1e899%3A0x9be95d4c335c090e!2z0LLRg9C70LjRhtGPINCV0L3RgtGD0LfRltCw0YHRgtGW0LIsIDgsINCn0LXRgNC90ZbQstGG0ZYsINCn0LXRgNC90ZbQstC10YbRjNC60LAg0L7QsdC70LDRgdGC0YwsIDU4MDAw!5e0!3m2!1suk!2sua!4v1657721232226!5m2!1suk!2sua" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
